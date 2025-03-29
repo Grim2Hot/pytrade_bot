8abf2b3460e71da634fe07edd9d5c2ac1d6aae7d
alexs <alex.sweet121@yahoo.com>
2025-03-29 11:14:22 +0000

    Changed Indicator to literal/series values for different return values of calculate function

--- modules/indicators/base.py
+++ modules/indicators/base.py
@@ -3,12 +3,23 @@ import pandas
 
 
 
-class Indicator(ABC):
+class SeriesIndicator(ABC):
     def __init__(self, data: pandas.DataFrame):
         self.data = data
 
     @abstractmethod
     def calculate(self) -> pandas.DataFrame:
+        """
+        Adds the calculated field to the dataframe parsed to the class.
+        """
+        pass
+
+class LiteralIndicator(ABC):
+    def __init__(self, data: pandas.DataFrame):
+        self.data = data
+
+    @abstractmethod
+    def calculate(self) -> int:
         """
         Adds the calculated field to the dataframe parsed to the class.
         """
