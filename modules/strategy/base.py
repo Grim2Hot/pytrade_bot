dda0cde2e30723ac7ead55c2f4849ea7d3a1f479
alexs <alex.sweet121@yahoo.com>
2025-03-29 12:59:34 +0000

    Added docstrings

--- modules/strategy/base.py
+++ modules/strategy/base.py
@@ -3,10 +3,17 @@ from abc import ABC, abstractmethod
 
 
 
+
 class Strategy(ABC):
+    """
+    Abstract base class for all trading strategies.
+    """
     def __init__(self, data : pandas.DataFrame, indicators : list = None):
         raise NotImplementedError("This is an abstract class. Please implement the methods in a subclass.")
     
     @abstractmethod
     def get_signal(self):
+        """
+        Returns a signal (BUY, SELL, HOLD) based on the strategy's logic.
+        """
         raise NotImplementedError("This is an abstract class. Please implement the methods in a subclass.")
