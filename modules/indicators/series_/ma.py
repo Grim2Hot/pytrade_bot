e2287e1d446dc38b02f0d41f84682bc4f5d04b94
alexs <alex.sweet121@yahoo.com>
2025-03-29 21:00:37 +0000

    Inserted config connectivity for SMA

--- modules/indicators/series_/ma.py
+++ modules/indicators/series_/ma.py
@@ -1,5 +1,6 @@
 from ..base import SeriesIndicator
 from ...signal.trend import Trend
+import yaml
 import pandas
 
 
@@ -12,9 +13,14 @@ class SMA(SeriesIndicator):
     Simple Moving Average (SMA).
     SMA is the average of a set of prices over a specified number of periods.
     """
-    def __init__(self, data: pandas.DataFrame, period: int = 14):
+    def __init__(self, data: pandas.DataFrame, period: int = 14, config: dict = None):
         super().__init__(data)
         self.period = period
+        self.config = config if config else {}
+        self.str_up = self.config.get('strong_up_threshold', 0.2)
+        self.up = self.config.get('up_threshold', 0.05)
+        self.str_down = self.config.get('strong_down_threshold', -0.2)
+        self.down = self.config.get('down_threshold', -0.05)
 
     def calculate(self):
         """
@@ -31,6 +37,13 @@ class SMA(SeriesIndicator):
     
     def get_trend(self, window: int = 10) -> Trend:
         """
+        Returns the trend of the SMA over the specified window.
+        The trend is determined by the percentage change over the specified window.
+        The values are placeholders for being inserted by the config. 
+        They represent small values, suitable for high resolution data, like minutes or seconds. 
+        For 5, 10, 15 minute data, the values should be adjusted to be larger.
+
+        @param window: The number of periods to use for the trend calculation. Default is 10.
         """
         sma_col = f'SMA_{self.period}'
         if sma_col not in self.data.columns:
@@ -42,13 +55,13 @@ class SMA(SeriesIndicator):
 
         pct_change = (recent_sma.iloc[-1] / recent_sma.iloc[0] - 1) * 100
 
-        if pct_change > 5:
+        if pct_change > self.str_up:
             return Trend.STR_UP
-        elif pct_change > 1:
+        elif pct_change > self.up:
             return Trend.UP
-        elif pct_change < -5:
+        elif pct_change < self.down:
             return Trend.STR_DOWN
-        elif pct_change < -1:
+        elif pct_change < self.str_down:
             return Trend.DOWN
         else:
             return Trend.NONE
